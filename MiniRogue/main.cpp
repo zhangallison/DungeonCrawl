8f1d1f4a9f6d1ba9d78de1d6b9f1fb6a80246968
zhangallison <57781635+zhangallison@users.noreply.github.com>
2020-10-15 14:10:15 -0700

    Initial Commit

---
+++ MiniRogue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MiniRogue
+//
+//  Created by Allison Zhang on 10/15/20.
+//  Copyright © 2020 Allison Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
